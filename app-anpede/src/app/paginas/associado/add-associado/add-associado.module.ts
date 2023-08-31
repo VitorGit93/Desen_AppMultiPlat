7a324ed750e5dcd86efc731527eae7a0b50b3049
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-08-30 22:06:16 -0400

    📝 Atualização projeto Anpede

--- app-anpede/src/app/paginas/associado/add-associado/add-associado.module.ts
+++ app-anpede/src/app/paginas/associado/add-associado/add-associado.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { AddAssociadoPageRoutingModule } from './add-associado-routing.module';
 
-import { FolderPage } from './folder.page';
+import { AddAssociadoPage } from './add-associado.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    AddAssociadoPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [AddAssociadoPage]
 })
-export class FolderPageModule {}
+export class AddAssociadoPageModule {}
